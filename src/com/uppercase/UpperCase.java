c5e5135567d709d7e85e4e17f9c46d2a6b2f4bb3
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 11:07:00 +0530

    pushed by sundarraj

---
+++ src/com/uppercase/UpperCase.java
@@ -0,0 +1,13 @@
+package com.uppercase;
+
+public class UpperCase {
+	public static void main(String[] args)
+    {
+        String str = "get the future you want";
+        
+        String upper = str.toUpperCase();
+
+        System.out.println("Entered String: " + str);
+        System.out.println("Lowercase String: " + upper);
+    }
+}
